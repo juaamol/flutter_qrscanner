613bca91f6521adbb6857d202f54a3970cad7edc
juaamol <jambulud@gmail.com>
2019-06-14 21:03:55 +0200

    Bottom navigation bar

--- lib/src/pages/home.page.dart
+++ lib/src/pages/home.page.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_qrscanner/src/pages/locations.page.dart';
+import 'package:flutter_qrscanner/src/pages/maps.page.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: _callPage(currentIndex),
+      bottomNavigationBar: _createBottomNavBar(),
     );
+  }
+
+  Widget _createBottomNavBar() {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: (int index) {
+        setState(() {
+         currentIndex = index; 
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.map), title: Text('Locations')),
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('Locations'))
+      ],
+    );
+  }
+
+  Widget _callPage(int actualPage) {
+    switch (actualPage) {
+      case 0:
+        return MapsPage();
+      case 1:
+        return LocationsPage();
+      default:
+        return MapsPage();
+    }
   }
 }
