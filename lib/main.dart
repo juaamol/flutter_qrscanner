ab0a94b6817ca3bc173c35d254e0cd3afd3865ba
juaamol <jambulud@gmail.com>
2019-06-14 20:32:23 +0200

    Basic home and page main

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_qrscanner/src/pages/home.page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QRReader',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomePage()
+      },
+    );
+  }
+}
